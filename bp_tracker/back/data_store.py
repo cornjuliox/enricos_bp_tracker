83d4ac9449ad86134279be4e15b929f445513b9b
Enrico Jr Tuvera <cornjuliox@users.noreply.github.com>
2022-11-12 19:01:19 +0800

    finally getting around to adding debugging statements

--- bp_tracker/back/data_store.py
+++ bp_tracker/back/data_store.py
@@ -11,7 +11,7 @@ class FlatFileWriter():
         self._path: pathlib.Path = path
         self._backup: pathlib.Path = self._path.parent / (self._path.name + ".bak")
 
-    def retrieve(self) -> list[dict]:
+    def retrieve(self) -> list[dict[Any, Any]]:
         with self._path.open("r") as F:
             try:
                 bpdata: list[dict[Any, Any]] = json.loads(F.read())
@@ -26,14 +26,16 @@ class FlatFileWriter():
 
 class BPDataStore():
     def __init__(self, path: pathlib.Path):
-        self._writer = FlatFileWriter(path)
+        # self._writer = FlatFileWriter(path)
         # NOTE: maybe a separate FileHandler class for this? 
         self.path: pathlib.Path = path
         with self.path.open("r") as F:
             try:
                 self._bpdata: list[dict[Any, Any]] = json.loads(F.read())
+                logger.debug("Existing file content found and loaded.")
             except json.decoder.JSONDecodeError:
                 self._bpdata = [] 
+                logger.debug("No existing file content found, starting with an empty list.")
 
         logger.debug(f"self._bpdata empty? {len(self._bpdata)}")
 
@@ -43,28 +45,38 @@ class BPDataStore():
         return max(minimum, min(val, maximum))
 
     def _commit(self):
+        logger.info("Committing new records to file.")
         with self.path.open("w") as F:
             F.write(json.dumps(self._bpdata))
     
     def latest(self, limit: int = 10, ascending: bool = False) -> list[dict]:
         limit = self._clamp(1, limit, len(self._bpdata))
+        logger.info("Retrieving latest records.")
+        logger.debug(f"limit set to: {limit}")
 
         if ascending:
+            logger.info("Returning records in ascending order, by datetime.")
             prepped: list[dict] = sorted(self._bpdata, key=lambda x: x["timestamp"])
         else:
+            logger.info("Returning records in descending order, by datetime.")
             prepped = sorted(self._bpdata, key=lambda x: x["timestamp"])[::-1]
 
         return prepped[:limit]
 
     def all(self):
+        logger.info("Returning ALL records. Good luck reading all that.")
         return self._bpdata
 
     def date_range(self, start: int, end: int) -> list[dict]:
+        logger.info("Returning records for a specific date range.")
+        logger.debug(f"start time: {start}")
+        logger.debug(f"end time: {end}")
         prepped: list[dict] = [x for x in self._bpdata if x["timestamp"] >= start and x["timestamp"] <= end]
         prepped = sorted(prepped, key=lambda x: x["timestamp"])[::-1]
         return prepped
 
     def add(self, sys: int, dia: int, pulse: int, notes: int, timestamp: int):
+        logger.info("Adding a new entry")
         data: dict = {
             "sys": sys,
             "dia": dia,
@@ -72,12 +84,15 @@ class BPDataStore():
             "notes": notes,
             "timestamp": timestamp
         }
+        logger.debug(f"raw_data: {data}")
 
         self._bpdata.append(data)
         self._commit()
 
     # NOTE: There should never be a need to 'edit' an entry, I hope.
     def remove(self, timestamp: int):
+        logger.info("Removing an entry based on timestamp.")
+        logger.debug(f"timestamp: {timestamp}")
         new_list: list[dict] = [x for x in self._bpdata if x.get("timestamp") != timestamp]
         self._bpdata = new_list
         self._commit()
